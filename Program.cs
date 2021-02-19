bdecb45bb97db3a85d0dcdeeaae42016a93bdb96
Mhsoun <m.mhsoon@gmail.com>
2021-02-19 01:02:57 +0100

    fixed the and and or

--- Program.cs
+++ Program.cs
@@ -3,82 +3,85 @@ class Program
 {
     static void Main()
     {
-        string str32 = "????????????????????????????????";
-        char[] chr32 = str32.ToCharArray();
-        //foreach(char c in chr32)
-        //{
-        //    Console.WriteLine(c);
-        //}
-        //Console.WriteLine("OUT WHILE");
         int n = 1;
-        while (n!=0)
+        while (n != 0)
         {
             n = int.Parse(Console.ReadLine());
+            string str32 = "????????????????????????????????";
+            char[] chr32 = str32.ToCharArray();
             for (int x = 1; x <= n; x++)
             {
-                Console.WriteLine("entered for x = " + x);
                 string instr = Console.ReadLine();
                 string[] split = instr.Split(new char[] { ' ' }, StringSplitOptions.None);
-                foreach (string i in split)
-                {
-                    Console.WriteLine("split content " + i);
-                }
-
                 switch (split[0])
                 {
                     case "CLEAR":
-                        int i = int.Parse(split[1]);
-                        chr32[i] = '0';
-                        //foreach (char c in chr32)
-                        //{
-                        //    Console.WriteLine(c);
-                        //}
+                        int clearI = 31- int.Parse(split[1]);
+                        chr32[clearI] = '0';
                         break;
-                    case "SET":
-                        i = int.Parse(split[1]);
-                        chr32[i] = '1';
-                        break;
-                    case "OR":
-                        i = int.Parse(split[1]);
-                        int j = int.Parse(split[2]);
-                        if ((chr32[i] != '?') && (chr32[j] !='?'))
-                        {
-                            if (!((chr32[i].Equals('0')) && (chr32[j].Equals('0'))))
-                            {
-                                chr32[i] = '1';
-                            }
-                            else
-                            {
-                                chr32[i] = '0';
-                            }
-                        }
-                        break;
-                    case "AND":
-                        i = int.Parse(split[1]);
-                        j = int.Parse(split[2]);
-                        if ((chr32[i] != '?') && (chr32[j] != '?'))
-                        {
-                            if (((chr32[i].Equals('1')) && (chr32[j].Equals('1'))))
-                            {
-                                chr32[i] = '1';
-                            }
-                            else
-                            {
-                                chr32[i] = '0';
-                            }
 
+                    case "SET":
+                        int setI = 31- int.Parse(split[1]);
+                        chr32[setI] = '1';
+                        break;
+
+                    case "OR":
+                        int orI = 31- int.Parse(split[1]);
+                        int orJ = 31- int.Parse(split[2]);
+                        if ((chr32[orI] != '?') || (chr32[orJ] != '?'))
+                        {
+                            if (chr32[orI].Equals('1') || chr32[orJ].Equals('1'))
+                            {
+                                chr32[orI] = '1';
+                            }
+                            else if (chr32[orI].Equals('0') && chr32[orJ].Equals('0'))
+                            {
+                                chr32[orI] = '0';
+                            }
+                            else
+                            {
+                                chr32[orI] = '?';
+                            }
+                        }
+                        else
+                        {
+                            chr32[orI] = '?';
                         }
                         break;
+
+                    case "AND":
+                        int andI = 31- int.Parse(split[1]);
+                        int andJ = 31- int.Parse(split[2]);
+                        if ((chr32[andI] != '?') && (chr32[andJ] != '?'))
+                        {
+                            if (chr32[andI].Equals('1') && chr32[andJ].Equals('1'))
+                            {
+                                chr32[andI] = '1';
+                            }
+                            else if (chr32[andI].Equals('0') || chr32[andJ].Equals('0'))
+                            {
+                                chr32[andI] = '0';
+                            }
+                            else
+                            {
+                                chr32[andI] = '?';
+                            }
+                        } else
+                        {
+                            chr32[andI] = '?';
+                        }
+                        break;
+
                     case "0":
-                            n = 0;
+                        n = 0;
                         break;
                 }
+                if (x == n)
+                {
+                    string result = new string(chr32);
+                    Console.WriteLine(result);
+                }
             }
-            //foreach (char c in chr32)
-            //{
-            //    Console.Write(c);
-            //}
-            Console.WriteLine("Out of Switch");
         }
     }
 }
