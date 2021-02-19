2aa8381d2dd9865f5e8b57e74b912ae9fe258e4f
Mhsoun <m.mhsoon@gmail.com>
2021-02-19 14:49:05 +0100

    fixed all bugs and code accepted

--- Program.cs
+++ Program.cs
@@ -28,44 +28,41 @@ class Program
                     case "OR":
                         int orI = 31- int.Parse(split[1]);
                         int orJ = 31- int.Parse(split[2]);
-                        if ((chr32[orI] != '?') || (chr32[orJ] != '?'))
+                        if ((chr32[orI] == '?') || (chr32[orJ] == '?'))
                         {
                             if (chr32[orI].Equals('1') || chr32[orJ].Equals('1'))
                             {
                                 chr32[orI] = '1';
-                            }
-                            else if (chr32[orI].Equals('0') && chr32[orJ].Equals('0'))
+                            }  else if (chr32[orI].Equals('0') || chr32[orJ].Equals('0'))
                             {
-                                chr32[orI] = '0';
-                            }
-                            else
+                                chr32[orI] = '?';
+                            } else
                             {
                                 chr32[orI] = '?';
                             }
                         }
-                        else
+                        else if (chr32[orI].Equals('1') || chr32[orJ].Equals('1'))
                         {
-                            chr32[orI] = '?';
-                        }
+                            chr32[orI] = '1';
+                        } else
+                        {
+                            chr32[orI] = '0';
+                        } 
                         break;
 
                     case "AND":
                         int andI = 31- int.Parse(split[1]);
                         int andJ = 31- int.Parse(split[2]);
-                        if ((chr32[andI] != '?') && (chr32[andJ] != '?'))
+                        if ((chr32[andI] == '1') && (chr32[andJ] == '1'))
                         {
-                            if (chr32[andI].Equals('1') && chr32[andJ].Equals('1'))
-                            {
-                                chr32[andI] = '1';
-                            }
-                            else if (chr32[andI].Equals('0') || chr32[andJ].Equals('0'))
-                            {
-                                chr32[andI] = '0';
-                            }
-                            else
-                            {
-                                chr32[andI] = '?';
-                            }
+                            chr32[andI] = '1';
+                        }
+                        else if ((chr32[andI] == '0') || (chr32[andJ] == '0'))
+                        {
+                            chr32[andI] = '0';
+                        } else if ((chr32[andI] == '?') && (chr32[andJ] == '?'))
+                        {
+                            chr32[andI] = '?';
                         } else
                         {
                             chr32[andI] = '?';
@@ -85,4 +82,3 @@ class Program
         }
     }
 }
-
